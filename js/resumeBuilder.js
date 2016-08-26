06f61103915500329e251fee96228f6811cc5ea0
amatellini <andrea.matellini@gmail.com>
2016-08-26 17:54:43 +0200

    Progress 20160826-1754

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,7 @@ var bio = {
         "email": "andrea.matellini@gmail.com",
         "github": "https://github.com/amatellini",
         "linkedin": "https://it.linkedin.com/in/andrea-matellini-3bb804b",
-        "twitter": "",
+        //"twitter": "",
         "location": "Pavia, Italy"
     },
     "welcomeMessage": "Hi there, my name is Andrea and this is my online resume!",
@@ -85,18 +85,24 @@ bio.display = function() {
 	var gitHubLink = "<a href=" + bio.contacts.github + " style=\"display: inherit\">" + gitHubName + "</a>";
 	var formattedGitHub = HTMLgithub.replace("%data%", gitHubLink);
 
+    var mailTo = "<a href=mailto:" + bio.contacts.email + " style=\"display: inherit\">" + bio.contacts.email + "</a>";
+
 	$("#header")
         .prepend(HTMLheaderRole.replace("%data%", bio.role))
         .prepend(HTMLheaderName.replace("%data%", bio.name))
         .append(HTMLbioPic.replace("%data%", bio.biopic))
         .append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 
-	$("#topContacts")
-        .append(HTMLmobile.replace("%data%", bio.contacts.mobile))
-        .append(HTMLemail.replace("%data%", bio.contacts.email))
-        .append(formattedGitHub)
-        .append(HTMLlocation.replace("%data%", bio.contacts.location));
+	var topContacts = $("#topContacts");
+    topContacts.append(HTMLmobile.replace("%data%", bio.contacts.mobile))
+                .append(HTMLemail.replace("%data%", mailTo))
+                .append(formattedGitHub);
 
+    if (typeof bio.contacts.twitter !== "undefined") {
+        topContacts.append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+    }
+
+    $("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
 
 	if(bio.skills.length > 0) {
 		$("#header").append(HTMLskillsStart);
@@ -158,23 +164,33 @@ education.display = function() {
 //TODO
 // 1) complete all json object
 // 2) add more elemente in array object
-// 3) manage twitter optional field (also on footer)
+// 3) manage twitter optional field (also on footer) --> ok
 // 4) add images where required (placeholder or kittens)
 // 5) bootstrap and navbar ?
 // 6) add some css
 // 7) lint and beautify html/css/js
-// 8) enable google maps visualizing the various locations
+// 8) enable google maps visualizing the various locations --> ok
 // 9) verify project subscription rules
 // 10) manage if attribute exist then ?
 // 11) test empty arrays
-// 12) manage link as href in footer and mail as "mail to"
+// 12) manage link as href in footer and mail as "mail to" --> ok
+// 13) check doc for display: inherit
 
 function displayFooter() {
+    var letsConnect = $('#lets-connect');
+    var mailTo = "<a href=mailto:" + bio.contacts.email + " style=\"display: inherit\">" + bio.contacts.email + "</a>";
+    var linkedinLink = "<a href=" + bio.contacts.github + " style=\"display: inherit\">" + bio.contacts.linkedin + "</a>";
+
     var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedEmail = HTMLemail.replace("%data%", mailTo);
     var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-    var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
-    $('#lets-connect').append(formattedMobile, formattedEmail, formattedTwitter, formattedLinkedin);
+    var formattedLinkedinLink = HTMLlinkedin.replace("%data%", linkedinLink);
+
+    letsConnect.append(formattedMobile, formattedEmail, formattedLinkedinLink);
+
+    if (typeof bio.contacts.twitter !== "undefined") {
+        letsConnect.append(formattedTwitter);
+    }
 }
 
 bio.display();
